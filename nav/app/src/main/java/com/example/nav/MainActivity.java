c2ecf97f5fa6f98c3cdaf7490ed39d1faae3a07d
jhc-creat <643594626@qq.com>
2021-10-16 22:02:51 +0800

    nav bar

--- nav/app/src/main/java/com/example/nav/MainActivity.java
+++ nav/app/src/main/java/com/example/nav/MainActivity.java
@@ -14,6 +14,7 @@ public class MainActivity extends AppCompatActivity implements RadioGroup.OnChec
     private RadioGroup rg_tab_bar;
     private RadioButton rb_channel;
 
+    //Fragment Object
 
     private Fragment fg1,fg2,fg3,fg4,fg5;
     private FragmentManager fManager;
@@ -25,6 +26,7 @@ public class MainActivity extends AppCompatActivity implements RadioGroup.OnChec
         fManager = getSupportFragmentManager();
         rg_tab_bar = (RadioGroup) findViewById(R.id.rg_tab_bar);
         rg_tab_bar.setOnCheckedChangeListener(this);
+        //获取第一个单选按钮，并设置其为选中状态
         rb_channel = (RadioButton) findViewById(R.id.diary);
         rb_channel.setChecked(true);
     }
@@ -45,7 +47,11 @@ public class MainActivity extends AppCompatActivity implements RadioGroup.OnChec
                 fg2 = new fg_profile();
                 fTransaction.add(R.id.ly_content,fg2);
                 break;
-                case R.id.retriever:
+            case R.id.add:
+                fg3 = new fg_add();
+                fTransaction.add(R.id.ly_content,fg3);
+                break;
+            case R.id.retriever:
                 fg4 = new fg_plan();
                 fTransaction.add(R.id.ly_content,fg4);
                 break;
@@ -57,9 +63,11 @@ public class MainActivity extends AppCompatActivity implements RadioGroup.OnChec
         fTransaction.commit();
     }
 
+    //隐藏所有Fragment
     private void hideAllFragment(FragmentTransaction fragmentTransaction){
         if(fg1 != null)fragmentTransaction.hide(fg1);
         if(fg2 != null)fragmentTransaction.hide(fg2);
+        if(fg3 != null)fragmentTransaction.hide(fg3);
         if(fg4 != null)fragmentTransaction.hide(fg4);
         if(fg5 != null)fragmentTransaction.hide(fg5);
     }
